826d5c8301bc8a5247b1771f7c37953da936e4b1
Michael Schwartz <mikethedj4@yahoo.com>
2017-01-14 14:06:13 -0600

    Added difficulties

--- game/js/randomskate.js
+++ game/js/randomskate.js
@@ -29,8 +29,235 @@ setInterval(countDown, 25);
 })(jQuery);
 
 var trickNumber = 0,
-    Tricks = [
+    SimpleTricks = [
       "WildCard (Player chooses trick)",
+      "ollie",
+      "fakie ollie",
+      "manual",
+      "fakie manual",
+      
+      "kickflip",
+      "heelflip",
+      "fs pop-shuv-it",
+      "bs pop-shuv-it",
+      "fs 180",
+      "bs 180",
+      "pressureflip",
+      
+      "fakie kickflip",
+      "fakie heelflip",
+      "fakie fs pop-shuv-it",
+      "fakie bs pop-shuv-it",
+      "fakie fs 180",
+      "fakie bs 180",
+      "fakie pressureflip"
+    ],
+    SimpleSwitchTricks = [
+      "WildCard (Player chooses switch trick)",
+      "switch ollie",
+      "switch kickflip",
+      "switch heelflip",
+      "switch fs pop-shuv-it",
+      "switch bs pop-shuv-it",
+      "switch fs 180",
+      "switch bs 180",
+      "switch manual",
+      "switch pressureflip",
+      
+      "nollie",
+      "nollie kickflip",
+      "nollie heelflip",
+      "nollie fs pop-shuv-it",
+      "nollie bs pop-shuv-it",
+      "nollie fs 180",
+      "nollie bs 180",
+      "nose manual",
+      "nollie pressureflip"
+    ],
+    IntermediateTricks = [
+      "WildCard (Player chooses trick)",
+      "ollie",
+      "switch ollie",
+      "nollie",
+      "fakie ollie",
+      
+      "fs 180",
+      "switch fs 180",
+      "nollie fs 180",
+      "fakie fs 180",
+
+      "bs 180",
+      "switch bs 180",
+      "nollie bs 180",
+      "fakie bs 180",
+
+      "fs 360",
+      "switch fs 360",
+      "nollie fs 360",
+      "fakie fs 360",
+
+      "bs 360",
+      "switch bs 360",
+      "nollie bs 360",
+      "fakie bs 360",
+
+      "pop-shuv-it",
+      "switch pop-shuv-it",
+      "nollie pop-shuv-it",
+      "fakie pop-shuv-it",
+
+      "fs pop-shuv-it",
+      "switch fs pop-shuv-it",
+      "nollie fs pop-shuv-it",
+      "fakie fs pop-shuv-it",
+
+      "360 pop-shuv-it",
+      "switch 360 pop-shuv-it",
+      "nollie 360 pop-shuv-it",
+      "fakie 360 pop-shuv-it",
+
+      "360 fs pop-shuv-it",
+      "switch fs 360 pop-shuv-it",
+      "nollie fs 360 pop-shuv-it",
+      "fakie fs 360 pop-shuv-it",
+
+      "kickflip",
+      "switch kickflip",
+      "nollie kickflip",
+      "fakie kickflip",
+
+      "fs kickflip",
+      "switch fs kickflip",
+      "nollie fs kickflip",
+      "fakie fs kickflip",
+
+      "bs kickflip",
+      "switch bs kickflip",
+      "nollie bs kickflip",
+      "fakie bs kickflip",
+
+      "double kickflip",
+      "switch double kickflip",
+      "nollie double kickflip",
+      "fakie double kickflip",
+
+      "fs double kickflip",
+      "switch fs double kickflip",
+      "nollie fs double kickflip",
+      "fakie fs double kickflip",
+
+      "bs double kickflip",
+      "switch bs double kickflip",
+      "nollie bs double kickflip",
+      "fakie bs double kickflip",
+
+      "heelflip",
+      "switch heelflip",
+      "nollie heelflip",
+      "fakie heelflip",
+
+      "fs heelflip",
+      "switch fs heelflip",
+      "nollie fs heelflip",
+      "fakie fs heelflip",
+
+      "bs heelflip",
+      "switch bs heelflip",
+      "nollie bs heelflip",
+      "fakie bs heelflip",
+
+      "double heelflip",
+      "switch double heelflip",
+      "nollie double heelflip",
+      "fakie double heelflip",
+
+      "fs double heelflip",
+      "switch fs double heelflip",
+      "nollie fs double heelflip",
+      "fakie fs double heelflip",
+
+      "bs double heelflip",
+      "switch bs double heelflip",
+      "nollie bs double heelflip",
+      "fakie bs double heelflip",
+
+      "varial kickflip",
+      "switch varial kickflip",
+      "nollie varial kickflip",
+      "fakie varial kickflip",
+
+      "varial heelflip",
+      "switch varial heelflip",
+      "nollie varial heelflip",
+      "fakie varial heelflip",
+
+      "360flip",
+      "switch 360flip",
+      "nollie 360flip",
+      "fakie 360flip",
+
+      "hardflip",
+      "switch hardflip",
+      "nollie hardflip",
+      "fakie hardflip",
+
+      "inward heelflip",
+      "switch inward heelflip",
+      "nollie inward heelflip",
+      "fakie inward heelflip",
+
+      "pressureflip",
+      "switch pressureflip",
+      "nollie pressureflip",
+      "fakie pressureflip",
+
+      "360 pressureflip",
+      "switch 360 pressureflip",
+      "nollie 360 pressureflip",
+      "fakie 360 pressureflip",
+
+      "bs big spin (bs pop-shuv-it + fs 180)",
+      "switch bs big spin (bs pop-shuv-it + fs 180)",
+      "nollie bs big spin (bs pop-shuv-it + fs 180)",
+      "fakie bs big spin (bs pop-shuv-it + fs 180)",
+
+      "fs big spin (fs pop-shuv-it + bs 180)",
+      "switch fs big spin (fs pop-shuv-it + bs 180)",
+      "nollie fs big spin (fs pop-shuv-it + bs 180)",
+      "fakie fs big spin (fs pop-shuv-it + bs 180)",
+
+      "late backfoot kickflip",
+      "switch late backfoot kickflip",
+      "nollie late backfoot kickflip",
+      "fakie late backfoot kickflip",
+
+      "late fs shuv-it",
+      "switch late fs shuv-it",
+      "nollie late fs shuv-it",
+      "fakie late fs shuv-it",
+
+      "manual",
+      "switch manual",
+      "nollie manual",
+      "fakie manual",
+
+      "manual ollie",
+      "switch manual ollie",
+      "nollie manual nollie",
+      "fakie manual fakie ollie",
+
+      "manual kickflip",
+      "switch manual kickflip",
+      "nollie manual kickflip",
+      "fakie manual fakie kickflip"
+    ],
+    AdvancedTricks = [
+      "WildCard (Player chooses trick)",
+      "ollie",
+      "switch ollie",
+      "nollie",
+      "fakie ollie",
+      
       "fs 180",
       "switch fs 180",
       "nollie fs 180",
@@ -269,7 +496,22 @@ var trickNumber = 0,
       "late fs shuv-it",
       "switch late fs shuv-it",
       "nollie late fs shuv-it",
-      "fakie late fs shuv-it"
+      "fakie late fs shuv-it",
+
+      "manual",
+      "switch manual",
+      "nollie manual",
+      "fakie manual",
+
+      "manual ollie",
+      "switch manual ollie",
+      "nollie manual nollie",
+      "fakie manual fakie ollie",
+
+      "manual kickflip",
+      "switch manual kickflip",
+      "nollie manual kickflip",
+      "fakie manual fakie kickflip"
     ],
     playerName, playerStats, Winner,
     playerSTR, playerC, playerL,
@@ -280,12 +522,39 @@ var trickNumber = 0,
       }
     };
 
-function refreshDeck() {
-  randomTrick = Math.floor(Math.random() * Tricks.length);
-  $("[data-place=trick]").text(Tricks[randomTrick]);
+function refreshSimple() {
+  randomTrick = Math.floor(Math.random() * SimpleTricks.length);
+  $("[data-place=trick]").text(SimpleTricks[randomTrick]);
   $(".followTrick").removeClass("followTrick");
 }
-refreshDeck();
+function refreshSimpleSwitch() {
+  randomTrick = Math.floor(Math.random() * SimpleSwitchTricks.length);
+  $("[data-place=trick]").text(SimpleSwitchTricks[randomTrick]);
+  $(".followTrick").removeClass("followTrick");
+}
+function refreshMed() {
+  randomTrick = Math.floor(Math.random() * IntermediateTricks.length);
+  $("[data-place=trick]").text(IntermediateTricks[randomTrick]);
+  $(".followTrick").removeClass("followTrick");
+}
+function refreshPro() {
+  randomTrick = Math.floor(Math.random() * AdvancedTricks.length);
+  $("[data-place=trick]").text(AdvancedTricks[randomTrick]);
+  $(".followTrick").removeClass("followTrick");
+}
+
+$("input[name=difficulty]").on("change", function() {
+  if ( $("#simple").is(":checked") ) {
+    refreshSimple();
+  } else if ( $("#simpleswitch").is(":checked") ) {
+    refreshSimpleSwitch();
+  } else if ( $("#intermediate").is(":checked") ) {
+    refreshMed();
+  } else if ( $("#professional").is(":checked") ) {
+    refreshPro();
+  }
+  return false;
+}).trigger("change");
 
 // How To Play
 $("[data-action=howto]").click(function() {
@@ -295,6 +564,16 @@ $("[data-action=howto]").click(function() {
       msg4 = "Once a player has the letters S.K.A.T.E. they are out of the game.<br>",
       msg5 = "The last remaining player wins.";
   alertify.alert("<h2 class='tl'>"+ msg1 + msg2 + msg3 + msg4 + msg5 +"</h2>").set("basic", true);
+});
+
+// Confirm Difficulty
+$("[data-confirm=difficulty]").click(function() {
+  if ( !$("input[name=difficulty]").is(":checked") ) {
+    alertify.error("No difficulty selected");
+  } else {
+    $(".difficulty").fadeOut(250);
+    $("[data-area=players]").delay(250).fadeIn();
+  }
 });
 
 // Add Players
@@ -350,7 +629,7 @@ $("[data-confirm=land]").click(function() {
       $("[data-player=turn]").text($("[data-player="+ playerName +"]").parent().next().children().first().text());
       $("[data-player=points]").text($("[data-player="+ playerName +"]").parent().next().children().first().next().text());
       if ($("[data-player="+ playerName +"]").parent().next().children().first().hasClass("followTrick")) {
-        refreshDeck();
+        $("input[name=difficulty]").trigger("change");
         trickNumber = parseInt($("[data-trick=attempts]").text());
         $("[data-trick=attempts]").text(trickNumber + 1);
       }
@@ -358,7 +637,7 @@ $("[data-confirm=land]").click(function() {
       $("[data-player=turn]").text($("[data-place=player] li").first().children().first().text());
       $("[data-player=points]").text($("[data-place=player] li").first().children().first().next().text());
       if ($("[data-place=player] li").first().children().first().hasClass("followTrick")) {
-        refreshDeck();
+        $("input[name=difficulty]").trigger("change");
         trickNumber = parseInt($("[data-trick=attempts]").text());
         $("[data-trick=attempts]").text(trickNumber + 1);
       }
@@ -370,7 +649,7 @@ $("[data-confirm=miss]").click(function() {
   playerStats = $("[data-player="+ playerName +"]").next();
 
   if (!$(".followTrick").is(":visible")) {
-    refreshDeck();
+    $("input[name=difficulty]").trigger("change");
     trickNumber = parseInt($("[data-trick=attempts]").text());
     $("[data-trick=attempts]").text(trickNumber + 1);
   } else {
@@ -406,7 +685,7 @@ $("[data-confirm=miss]").click(function() {
     $("[data-player=turn]").text($("[data-player="+ playerName +"]").parent().next().children().first().text());
     $("[data-player=points]").text($("[data-player="+ playerName +"]").parent().next().children().first().next().text());
     if ($("[data-player="+ playerName +"]").parent().next().children().first().hasClass("followTrick")) {
-      refreshDeck();
+      $("input[name=difficulty]").trigger("change");
       trickNumber = parseInt($("[data-trick=attempts]").text());
       $("[data-trick=attempts]").text(trickNumber + 1);
     }
@@ -414,7 +693,7 @@ $("[data-confirm=miss]").click(function() {
     $("[data-player=turn]").text($("[data-place=player] li").first().children().first().text());
     $("[data-player=points]").text($("[data-place=player] li").first().children().first().next().text());
     if ($("[data-place=player] li").first().children().first().hasClass("followTrick")) {
-      refreshDeck();
+      $("input[name=difficulty]").trigger("change");
       trickNumber = parseInt($("[data-trick=attempts]").text());
       $("[data-trick=attempts]").text(trickNumber + 1);
     }
